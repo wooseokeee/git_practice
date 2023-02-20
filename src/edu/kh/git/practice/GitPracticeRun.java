d9fd1c56021994aabb2290d60bec0ed6ee447690
wooseokeeeee <dntjr1318@gmail.com>
2023-02-20 19:16:12 +0900

    첫 번째 커밋 생성

---
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -0,0 +1,10 @@
+package edu.kh.git.practice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요~");
+		System.out.println("Git 연습용 파일입니다.");
+
+	}
+}
