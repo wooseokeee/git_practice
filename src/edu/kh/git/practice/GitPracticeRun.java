118dd12af6f6cc881b81261b9ca4daa606669ab5
wooseokeeeee <dntjr1318@gmail.com>
2023-02-20 20:16:30 +0900

    깃 허브 연동 완료

--- src/edu/kh/git/practice/GitPracticeRun.java
+++ src/edu/kh/git/practice/GitPracticeRun.java
@@ -5,5 +5,6 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요~");
 		System.out.println("깃허브를 다시 생성해보겠습니다.");
+		System.out.println("깃허브 연동 완료");
 	}
 }
